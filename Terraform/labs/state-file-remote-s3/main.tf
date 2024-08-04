71f611847b241f689e793f185e9a836ef0f1dd0f
Alex Petrushchak <petrushchako@gmail.com>
2024-08-04 23:32:30 +0100

    Add main.tf content for docker image creation

--- Terraform/labs/state-file-remote-s3/main.tf
+++ Terraform/labs/state-file-remote-s3/main.tf
@@ -1,7 +1,20 @@
-terraform {
-  backend "s3' {
-    region = "us-east-1"
-    key = "terraformstatefile"
-    bucket = "myawesomes3bucket3344"
-    }
+provider "docker" {}
+
+resource "docker_image" "nginx-image" {
+  name = "nginx"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx-image.latest
+  name  = "nginx"
+  ports {
+    internal = 80
+    external = var.external_port
+    protocol = "tcp"
+  }
+}
+
+output "url" {
+  description = "Browser URL for container site"
+  value       = join(":", ["http://localhost", tostring(var.external_port)])
 }
