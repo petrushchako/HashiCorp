fd009586b270187b5c99eb69c0f1bd72b44c5d98
Alex Petrushchak <petrushchako@gmail.com>
2024-08-07 23:47:25 +0100

    Add vpc module variable comments

--- Terraform/labs/module-vpc-s3/terraform-vpc/variables.tf
+++ Terraform/labs/module-vpc-s3/terraform-vpc/variables.tf
@@ -1,19 +1,27 @@
+# Variable to specify the AWS region for VPC deployment.
 variable "region" {
-  description = "The AWS region to deploy to"
+  description = "The AWS region where the VPC and its resources will be created."
   type        = string
+  # Must be defined in the root module's terraform.tfvars file or passed directly in the Terraform command.
 }
 
+# Variable to define the CIDR block for the VPC.
 variable "vpc_cidr" {
-  description = "The CIDR block for the VPC"
+  description = "The CIDR block for the VPC, defining its IP address range."
   type        = string
+  # Must be defined in the root module's terraform.tfvars file or passed directly in the Terraform command.
 }
 
+# Variable to provide a list of CIDR blocks for public subnets in the VPC.
 variable "public_subnets" {
-  description = "A list of public subnet CIDR blocks"
+  description = "A list of CIDR blocks for the VPC's public subnets."
   type        = list(string)
+  # Must be defined in the root module's terraform.tfvars file or passed directly in the Terraform command.
 }
 
+# Variable to specify a list of availability zones for resource deployment.
 variable "azs" {
-  description = "A list of availability zones"
+  description = "A list of availability zones in the AWS region for deploying resources."
   type        = list(string)
+  # Must be defined in the root module's terraform.tfvars file or passed directly in the Terraform command.
 }
