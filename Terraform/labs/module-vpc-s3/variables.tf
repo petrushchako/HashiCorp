bb1f0e3ada5b4f746f1edfa5b0c278363efba87f
Alex Petrushchak <petrushchako@gmail.com>
2024-08-07 23:48:11 +0100

    Add root variables comments

--- Terraform/labs/module-vpc-s3/variables.tf
+++ Terraform/labs/module-vpc-s3/variables.tf
@@ -1,5 +1,6 @@
+# Variable to define the AWS region where resources will be deployed.
 variable "region" {
-  description = "The AWS region to deploy to"
+  description = "The AWS region where all resources will be deployed."
   type        = string
-  default     = "us-west-2"
+  default     = "us-west-2"  # Default value. Can be overridden in terraform.tfvars or via command line arguments.
 }
