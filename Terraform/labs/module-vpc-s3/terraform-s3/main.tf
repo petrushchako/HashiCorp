7b828009fa6dc0a6f0a807b9aa9b78ee8a5b2627
Alex Petrushchak <petrushchako@gmail.com>
2024-08-07 23:37:47 +0100

    Add comment to s3 module main.tf

--- Terraform/labs/module-vpc-s3/terraform-s3/main.tf
+++ Terraform/labs/module-vpc-s3/terraform-s3/main.tf
@@ -1,13 +1,15 @@
+# Define the S3 bucket resource
 resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
+  bucket = var.bucket_name  # Name of the S3 bucket
+  acl    = "private"  # Access control list setting to make the bucket private
 
   tags = {
-    Name        = var.bucket_name
-    Environment = var.environment
+    Name        = var.bucket_name  # Tag for the bucket with its name
+    Environment = var.environment  # Tag for the bucket indicating the environment (e.g., dev)
   }
 }
 
+# Output the bucket ARN
 output "bucket_arn" {
-  value = aws_s3_bucket.bucket.arn
+  value = aws_s3_bucket.bucket.arn  # Return the ARN of the S3 bucket
 }
